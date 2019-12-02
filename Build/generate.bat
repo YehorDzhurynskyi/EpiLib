1a90ed2226cf319bdb47bb45bf28f568506b4083
Yehor Dzhurynskyi <y.a.dzhurynskyi@gmail.com>
2019-12-02 22:14:19 +0300

    build flow update

---
+++ Build/generate.bat
@@ -0,0 +1,46 @@
+@echo off
+
+:main
+  setlocal enabledelayedexpansion
+
+  call :get-ini %1 env workdir workdir
+  set workdir=%cd%\%workdir%
+
+  call :get-ini %1 env epibuilddir epibuilddir
+  set epibuilddir=%cd%\%epibuilddir%
+
+  call :get-ini %1 cmake initial_cache initial_cache
+  set initial_cache=%cd%\%initial_cache%
+
+  call :get-ini %1 thirdparty qtdir qtdir
+
+  cmake -G "Visual Studio 15 2017 Win64" -S %workdir% -B "%workdir%\_projects\Visual_Studio_15_2017_Win64" -DEPI_BUILD_DIR=%epibuilddir% -DQt5_DIR=%qtdir% -C %initial_cache%
+
+  goto :eof
+
+:get-ini <filename> <section> <key> <result>
+  set %~4=
+  setlocal
+  set insection=
+  for /f "usebackq eol=; tokens=*" %%a in ("%~1") do (
+    set line=%%a
+    if "!line:~0,1!"=="[" (
+      for /f "delims=[]" %%b in ("!line!") do (
+        if /i "%%b"=="%2" (
+          set insection=1
+        ) else (
+          endlocal
+        )
+      )
+    )
+    if defined insection (
+      for /f "tokens=1,* delims==" %%b in ("!line!") do (
+        if /i "%%b"=="%3" (
+          endlocal
+          set %~4=%%c
+          goto :eof
+        )
+      )
+    )
+  )
+  endlocal
